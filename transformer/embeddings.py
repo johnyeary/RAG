9254cb588eb580acf57131e06e239e0de73492bd
johnyeary <john.my.96@gmail.com>
2025-06-01 14:16:20 -0700

    init and embeddings updates

--- transformer/embeddings.py
+++ transformer/embeddings.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import math
 class TokenPositionalEmbedding(nn.Module):
     #positional encoding
-    def __init_(self,vocab_size,d_model,max_len=512):
+    def __init__(self,vocab_size,d_model,max_len=512):
         super().__init__()
         self.token_emb = nn.Embedding(vocab_size,d_model)
         #sine-cosine positional encodings
@@ -12,8 +12,8 @@ class TokenPositionalEmbedding(nn.Module):
         div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model))
       
        # interleave sin/cos encodings
-        pe[position,0::2] = torch.sin(position*div_term)
-        pe[position,1::2] = torch.cos(position*div_term)
+        pe[:,0::2] = torch.sin(position*div_term)
+        pe[:,1::2] = torch.cos(position*div_term)
         
         # want dim (1,max_len,d_model)
         pe = pe.unsqueeze(0)
@@ -32,7 +32,13 @@ class TokenPositionalEmbedding(nn.Module):
         tok_emb = self.token_emb(token_ids)
 
         # Slice positional encodings for seq_len
-        pos_emb - self.pos_enc[:,:seq_len,:]
+        pos_emb = self.pos_enc[:,:seq_len,:]
         
         # add together and return
         return tok_emb + pos_emb
+    
+    def positional_encoding(self,pos:int):
+        """
+        Return positional encoding vector for 'pos'
+        """
+        return self.pos_enc[0,pos,:]
